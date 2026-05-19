121d76b04ac8770cd3df24f7a216824daf0d6f0b
Ivanp2003 <andrespanchichavez@gmail.com>
2026-05-19 11:25:47 -0500

    Correción en respuesta al obtener HC

--- src/models/HistorialClinico.js
+++ src/models/HistorialClinico.js
@@ -697,7 +697,7 @@ const historialClinicoSchema = new mongoose.Schema({
 }, {
   timestamps: true,
   versionKey: false,
-  toJSON: { virtuals: true },
+  toJSON: { virtuals: false },
   toObject: { virtuals: true }
 });
 
