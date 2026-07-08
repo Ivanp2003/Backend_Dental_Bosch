006e1bd85e4b2c5b70eb1fdcd5cb32e850baa6db
Ivanp2003 <andrespanchichavez@gmail.com>
2026-07-08 09:39:15 -0500

    Correccion

---
+++ clean.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dental_bosch').then(async () => {
+  const Usuario = require('./src/models/Usuario');
+  await Usuario.deleteMany({ email: { $in: ['doctor@dentalbosch.com', 'paciente@dentalbosch.com'] } });
+  
+  const Doctor = require('./src/models/Doctor');
+  await Doctor.deleteMany({ especialidad: 'Odontología General' });
+  
+  const Paciente = require('./src/models/Paciente');
+  await Paciente.deleteMany({});
+  
+  console.log('Usuarios de prueba antiguos eliminados.');
+  
+  await require('./src/seeds/AdminDoctor')();
+  console.log('Nuevos usuarios recreados con contraseñas encriptadas y cédulas reales.');
+  mongoose.disconnect();
+});
